3aaa5f9152440b0a34c4d36e3df9655003c111db
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-06-13 19:14:32 +0200

    Class Texto

--- src/com/company/Texto.java
+++ src/com/company/Texto.java
@@ -1,4 +1,23 @@
 package com.company;
 
-public class Texto {
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Texto extends JPanel {
+    JLabel texto = new JLabel("HUNDIR LA FLOTA");
+
+    Texto() {
+        this.setBorder(BorderFactory.createMatteBorder(0, 0, 25, 0, new JPanel().getBackground()));
+
+        add(texto);
+        texto.setFont(this.getFont().deriveFont(17.0f));
+    }
+
+    //--METODOS---
+
+
+
+
 }
+
