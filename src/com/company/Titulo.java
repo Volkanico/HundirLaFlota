30bee6fd9b94928df7243ed02041f07023b5ad08
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-06-13 19:13:34 +0200

    Class Ventana y Titulo

--- src/com/company/Titulo.java
+++ src/com/company/Titulo.java
@@ -1,4 +1,37 @@
 package com.company;
 
-public class Titulo {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class Titulo extends JPanel {
+    JPanel barra;
+    JPanel panel_titulo = new JPanel();
+    JLabel titulo;
+
+    Titulo() {
+        barra = new JPanel();
+        setLayout(new GridLayout(2, 1));
+        panel_titulo.setBorder(new EmptyBorder(0, 0, 0, 0));
+        add(barra);
+
+        //------------------------------
+
+        add(panel_titulo);
+
+        this.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+        //TITULO
+        titulo = new JLabel("HUNDIR LA FLOTA - BY VOLKAN :)", SwingConstants.CENTER);
+        titulo.setFont(new Font("Arial Black", 1, 20));
+        titulo.setForeground(new Color(51, 133, 255));
+        titulo.setVisible(true);
+        panel_titulo.add(titulo);
+    }
 }
+
+
--- src/com/company/Ventana.java
+++ src/com/company/Ventana.java
@@ -1,4 +1,47 @@
 package com.company;
 
-public class Ventana {
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+
+    Juego juego;
+    Texto texto;
+    Titulo titulo;
+
+    Ventana() {
+        setTitle("Hundir la Flota 2.0");
+
+        //--CARACTERISTICAS DE LA VENTANA
+
+        this.setResizable(false);
+
+        this.setIconImage(new ImageIcon("src\\imagenes\\icono.png").getImage());
+
+        setBounds(100, 100, 490, 1010);
+
+        setLocationRelativeTo(null);
+
+        BorderLayout layout = new BorderLayout();
+        setLayout(layout);
+
+        juego = new Juego();
+        add(juego, BorderLayout.CENTER);
+
+        texto = new Texto();
+        add(texto, BorderLayout.SOUTH);
+
+        titulo = new Titulo();
+        add(titulo, BorderLayout.NORTH);
+
+        //-------------------------
+
+        setVisible(true);
+
+    }
+
+
 }
+
+
